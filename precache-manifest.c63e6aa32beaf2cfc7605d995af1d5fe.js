2ad639eabde729ad6bf01fff0238a59f8ff4704b
babloo <babloo.mishra@tothenew.com>
2019-07-17 12:02:27 +0530

    Updates

---
+++ precache-manifest.c63e6aa32beaf2cfc7605d995af1d5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6fd8334db275aca7067c1db5d7889d",
+    "url": "/motus/index.html"
+  },
+  {
+    "revision": "5688dc25e13011f58ae8",
+    "url": "/motus/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "4656b486352fbd3a9d61",
+    "url": "/motus/static/css/main.27d62e3e.chunk.css"
+  },
+  {
+    "revision": "5688dc25e13011f58ae8",
+    "url": "/motus/static/js/2.5904fa19.chunk.js"
+  },
+  {
+    "revision": "4656b486352fbd3a9d61",
+    "url": "/motus/static/js/main.ca4c1e5c.chunk.js"
+  },
+  {
+    "revision": "cd947d4601d87cbb0f1d",
+    "url": "/motus/static/js/runtime~main.65fc3baf.js"
+  }
+]);
